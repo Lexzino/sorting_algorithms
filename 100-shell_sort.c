0131dada4d8fff66f9c6c43f21b71d18fb78e796
Lexzino <alexudeze001@gmal.com>
2024-01-20 23:02:08 -0800

    shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Sort an array of integer using shell sort
+ * @array: An array of integers.
+ * @size: The size of the array.
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
+
+	for (; gap >= 1; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
+}
