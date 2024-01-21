108c796c4c17bf9fbee0d0bee0a427cd23a9a6f6
Lexzino <alexudeze001@gmal.com>
2024-01-20 23:06:59 -0800

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,25 +8,30 @@
 
 void shell_sort(int *array, size_t size)
 {
-	size_t gap, i, j;
+	unsigned int gap = 1, i, j;
+	int temp;
 
-	if (array == NULL || size < 2)
+	if (array == NULL)
 		return;
-
-	for (gap = 1; gap < (size / 3);)
+	if (size < 2)
+		return;
+	while (gap < size / 3)
 		gap = gap * 3 + 1;
 
-	for (; gap >= 1; gap /= 3)
+	while (gap > 0)
 	{
 		for (i = gap; i < size; i++)
 		{
+			temp = array[i];
 			j = i;
-			while (j >= gap && array[j - gap] > array[j])
+			while (j >= gap && array[j - gap] > temp)
 			{
-				swap_ints(array + j, array + (j - gap));
+				array[j] = array[j - gap];
 				j -= gap;
 			}
+			array[j] = temp;
 		}
 		print_array(array, size);
+		gap /= 3;
 	}
 }
